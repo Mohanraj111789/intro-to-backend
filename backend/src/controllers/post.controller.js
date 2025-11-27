107aaa33b19fa8550d2f1e56be503a068584423a
Mohanraj111789 <kit27.am30@gmail.com>
2025-11-27 22:37:56 +0530

    Understanding controller

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,15 +1,37 @@
-import {Post} from "../models/post.model.js";
-const createPost = async(req,res)=>{
-    try{
-    const {name,description,age} = req.body;
-    if(!name || !description || !age)
-    {
-        return res.status(400).json({message:"All fields are required"});
+import { Post } from "../models/post.model.js";
+
+const createPost = async (req, res) => {
+  try {
+    const { name, description, age } = req.body;
+
+    if (!name || !description || !age) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    const post = await Post.create({name,description,age});
-    return res.status(201).json({message:"Post created successfully",post});
-}catch(error){
-    res.status(500).json({message:"Server Error: " + error.message});
-}
-}
-export {createPost};
+
+    const post = await Post.create({ name, description, age });
+
+    return res.status(201).json({
+      message: "Post created successfully",
+      post,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server Error: " + error.message });
+  }
+};
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    return res.status(200).json({
+      message: "Posts fetched successfully",
+      posts,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server Error: " + error.message });
+  }
+};
+
+export { createPost, getPosts };
